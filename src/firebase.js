9419a29395f149c9d22e27395723086d79c64914
alihasnainramzan <ah7419617@gmail.com>
2025-01-26 21:22:23 +0500

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,65 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import {
+  getMessaging,
+  getToken,
+  onMessage,
+  isSupported,
+} from "firebase/messaging";
+import { getAuth } from "firebase/auth";
+const firebaseConfig = {
+  apiKey: "AIzaSyDgXR2J-W_yCdgOMbletHUtYSYKneo5gYk",
+  authDomain: "hurry-up-food-all.firebaseapp.com",
+  projectId: "hurry-up-food-all",
+  storageBucket: "hurry-up-food-all.appspot.com",
+  messagingSenderId: "74764409036",
+  appId: "1XA9J4Bkm6YNEFZ6p9C2NzWH5kMvCxukgRBYSxyV4y8AxYv",
+  measurementId: "G-FF4M0XCTFV",
+};
+const firebaseApp = !getApps().length
+  ? initializeApp(firebaseConfig)
+  : getApp();
+const messaging = (async () => {
+  try {
+    const isSupportedBrowser = await isSupported();
+    if (isSupportedBrowser) {
+      return getMessaging(firebaseApp);
+    }
+    return null;
+  } catch (err) {
+    return null;
+  }
+})();
+
+export const fetchToken = async (setTokenFound, setFcmToken) => {
+  return getToken(await messaging, {
+    vapidKey: "",
+  })
+    .then((currentToken) => {
+      if (currentToken) {
+        setTokenFound(true);
+        setFcmToken(currentToken);
+
+        // Track the token -> client mapping, by sending to backend server
+        // show on the UI that permission is secured
+      } else {
+        setTokenFound(false);
+        setFcmToken();
+        // shows on the UI that permission is required
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      // catch error while creating client token
+    });
+};
+
+export const onMessageListener = async () =>
+  new Promise((resolve) =>
+    (async () => {
+      const messagingResolve = await messaging;
+      onMessage(messagingResolve, (payload) => {
+        resolve(payload);
+      });
+    })()
+  );
+export const auth = getAuth(firebaseApp);
